36b32c87be1e7bb15ad046c77922a606b357cec0
delmy <dfajardob@miumg.edu.gt>
2024-03-17 20:38:37 -0600

    practica

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using practicadel16;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetoDemo0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // practica de progra orientada a objetos
+            // creo objeto de la clase y a esto se le llamo un objeto o instancia
+            //Estado son los valores de las propiedades de un objeto osea cuando se le asignan valores
+            Animal foca = new Animal( "foca", 0.50, 1.34, "hembra"); // todo los metodos tienen un metodo construcctor con el mismo nombre de la clase 
+            // la palabra new va porque crea un metodo construcctor  y este construcctor nos ayuda a ahorrar codigo
+            // la edad no va en el constructor ya que no siempre va a tener una misma edad  
+           // foca.nombre = " foca";
+          //  foca.altura = 0.50;
+           // foca.peso = 1.34;
+           // foca.sexo = "hembra" ;
+           // foca.edad = 2;
+            foca.Nombre = "otro nombre";
+
+
+
+            Console.WriteLine(" mi animal es: "+ foca);
+            //cuando le pido a la clase animal que  haga una accion en el programa pincipla  a eso se le llama mensaje 
+            //metodos de acceso son getters o setter 
+            foca.Comer(); 
+            Console.ReadKey();
+
+
+             
+             
+        }
+    }
+}
---
+++ animal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicadel16
+{
+    internal class Animal // esta clse solo puede ser usa en el main space
+                          //metodos 
+    {
+        private string nombre;
+        public string Nombre {
+            get { return nombre; }
+            set {  nombre = value; }
+        }
+        private double altura;
+        private double peso;
+        private string sexo;
+        public  int Edad { // permite que mi elemento tenga un encapsulado desde el principio 
+            get;set;
+        }
+
+        public string GetNombre()
+        { return nombre; }
+
+      // toadaas las clases tienen un metodo que se llama ToString()
+
+        public string GetPeso ( ) 
+        { return sexo; }
+        public void SetPeso() 
+        { return ; }
+        // creo metodo de comer cuando hago un metodo en este caso uso la palabra public  acompañado de void 
+        //pilares bases de la proga : encampsulamiento el cual se base enn proteger las propiedades de un objeto,
+        //como protejo el atributo nombre, altura,peso,sexo  eso se hace con la palabra private 
+        public Animal( string nombre, double altura, double peso, string sexo)
+        {//la palabra this sirve para diferenciar el nombre de clase con otra clase 
+            this .nombre = nombre;
+            this .altura = altura;
+            this .peso = peso;
+            this .sexo = sexo;
+         } 
+        public void Comer()
+        {
+            Console.WriteLine( "la " + nombre + "come pescado"); 
+        }
+         //permite devolver un string los atributos de una clase 
+        public override string ToString()
+        {
+            return "nombre" + nombre + "altura" + altura + " peso"+ peso + "edad" + Edad  ;
+        }
+
+
+
+    }
+}
