eeaea293e009a74985ac53a356d4a31b52ffc0b1
delmy <dfajardob@miumg.edu.gt>
2024-05-02 19:20:03 -0600

    los de  6 de abril

--- Clase-6deabril/Program.cs
+++ Clase-6deabril/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Clase_6deabril.Empleado2;
 
 namespace Clase_6deabril
 {
-    internal class Program
+     internal class Program
     {
         static void Main(string[] args)
         {
+            //herencia cuando las clases comparten los atributos 
+            // es cuando tenemos dos clases con atributos similares y las podemos generalizar= podemos agrupar
+            //los atributos que tienen; cuando no podemos aplicar herencia  cuando las dos clases no compaarten los mismos atributos 
+            //con solo uno que no tenga no se puede heredar, y hay que tomar en cuenta que deben de ser del mismo tipo ejemplo 
+            //un ejemplo es (la foto )
+            //la jerarquia de herencias es caundo hay una super clase osea la clase mayor, de esta se derivan las subclases y de estas subclases se derivan mas 
+            //pero es mejor 
+
             string Nombre = "Juan";
             int Edad = 30;
             int salario = 5000;
@@ -36,6 +45,11 @@ namespace Clase_6deabril
 
             Console.ReadKey();
             Console.ReadLine();
+
+            //una interfaz se denominan metodos abstracctos= no tiene logica implementada es decir que los metodos estan
+            //vacios 
+            //una interfaz es un contrato, siempre llevan la terminacion aude
+
         }
     }
 }
